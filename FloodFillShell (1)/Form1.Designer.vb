ea6d1d71934033fbc27530ec2637551f0b65e8bb
LukeWhitfield19 <48789849+LukeWhitfield19@users.noreply.github.com>
2021-01-10 20:57:03 +0000

    Add files via upload

---
+++ FloodFillShell (1)/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pBox = New System.Windows.Forms.PictureBox()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnClr = New System.Windows.Forms.Button()
+        Me.lblCurrentColour = New System.Windows.Forms.Label()
+        Me.lblCursorColour = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        CType(Me.pBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pBox
+        '
+        Me.pBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.pBox.Location = New System.Drawing.Point(12, 12)
+        Me.pBox.Name = "pBox"
+        Me.pBox.Size = New System.Drawing.Size(829, 688)
+        Me.pBox.TabIndex = 0
+        Me.pBox.TabStop = False
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnOpen.Location = New System.Drawing.Point(865, 12)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(122, 61)
+        Me.btnOpen.TabIndex = 1
+        Me.btnOpen.Text = "Open File"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnClose.Location = New System.Drawing.Point(865, 179)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(122, 60)
+        Me.btnClose.TabIndex = 2
+        Me.btnClose.Text = "Quit"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnClr
+        '
+        Me.btnClr.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnClr.Location = New System.Drawing.Point(869, 437)
+        Me.btnClr.Name = "btnClr"
+        Me.btnClr.Size = New System.Drawing.Size(118, 59)
+        Me.btnClr.TabIndex = 3
+        Me.btnClr.Text = "Pick Colour"
+        Me.btnClr.UseVisualStyleBackColor = True
+        '
+        'lblCurrentColour
+        '
+        Me.lblCurrentColour.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblCurrentColour.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCurrentColour.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblCurrentColour.Location = New System.Drawing.Point(893, 527)
+        Me.lblCurrentColour.Name = "lblCurrentColour"
+        Me.lblCurrentColour.Size = New System.Drawing.Size(61, 48)
+        Me.lblCurrentColour.TabIndex = 4
+        '
+        'lblCursorColour
+        '
+        Me.lblCursorColour.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblCursorColour.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCursorColour.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblCursorColour.Location = New System.Drawing.Point(893, 310)
+        Me.lblCursorColour.Name = "lblCursorColour"
+        Me.lblCursorColour.Size = New System.Drawing.Size(61, 48)
+        Me.lblCursorColour.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(889, 585)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Fill Colour"
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.Location = New System.Drawing.Point(865, 89)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(122, 61)
+        Me.btnSave.TabIndex = 7
+        Me.btnSave.Text = "Save File"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1003, 714)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblCursorColour)
+        Me.Controls.Add(Me.lblCurrentColour)
+        Me.Controls.Add(Me.btnClr)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.pBox)
+        Me.Name = "frmMain"
+        Me.Text = "FloodFill"
+        CType(Me.pBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pBox As PictureBox
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnClr As Button
+    Friend WithEvents lblCurrentColour As Label
+    Friend WithEvents lblCursorColour As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSave As Button
+End Class
---
+++ FloodFillShell (1)/Form1.vb
@@ -0,0 +1,118 @@
+﻿
+Imports System.Drawing
+' #############################################################################################
+' ### Work to do:
+' ###
+' ### Defining a new bitmap each time the user clicks, and when getting current pixel colour
+' ### is very memory inefficient. It is better to create a global bitmap variable, 
+' ### set the picture box to it when loaded, and operate from the single bitmap instance
+' ### flushing it back to the picturebox whenever appropriate.
+' ###
+' ### This MUST be done before you can write the floodfill, or it will chomp up meory after
+' ### a very short time
+' ###
+' ###
+' ### Another issue is the mouse move event on the image. If the form is resized past the image
+' ### an error will be generated when the sub attempts to get the current pixel colour
+' ### as there is no valid pixel within the grey part of the image
+' ###
+' ### This could be expanded to allow drawing of other shapes, etc.
+
+
+Public Class frmMain
+    Dim bmp As Bitmap
+    Dim gr As Graphics
+    Dim br As SolidBrush
+    ' global declarations can go here
+    Public CurrColour As New System.Drawing.Color
+    Public GotPic As Boolean = False
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' set default colours here
+        CurrColour = Color.Black
+
+        ShowColour()
+    End Sub
+
+    Public Sub ShowColour()
+        lblCurrentColour.BackColor = CurrColour
+    End Sub
+
+    Private Sub btnClr_Click(sender As Object, e As EventArgs) Handles btnClr.Click
+        Dim c As New ColorDialog
+        c.Color = CurrColour
+        If c.ShowDialog = DialogResult.OK Then
+            CurrColour = c.Color
+            ShowColour()
+        End If
+    End Sub
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        Dim GetFile As New OpenFileDialog
+        If GetFile.ShowDialog = DialogResult.OK Then
+            ' load file
+            pBox.Load(GetFile.FileName)
+            Controls.Add(pBox)
+            pBox.SizeMode = PictureBoxSizeMode.AutoSize
+            GotPic = True
+        End If
+    End Sub
+
+    Private Sub pBox_MouseUp(sender As Object, e As MouseEventArgs) Handles pBox.MouseUp
+        ' mouse up exposes x and y coordinates, whereas mouse click does not
+        If GotPic = False Then
+            MessageBox.Show("Sorry, please load a picture first", "Error - No Picture", MessageBoxButtons.OK)
+            Exit Sub
+        Else
+            ' get mouse position (i.e. click)
+            Dim pos As New Point(e.X, e.Y) ' lock point
+            Try
+                bmp = New Bitmap(pBox.Image) ' create a bitmap image (it will take dpi and resolution from loaded image)
+                gr = Graphics.FromImage(bmp) ' create a graphics surface on which to draw, as bitmaps cannot be directly manipluated
+                br = New SolidBrush(CurrColour) ' brush is passed into rectangle fill
+                FloodFill(pos, bmp.GetPixel(pos.X, pos.Y))
+
+                'gr.FillRectangle(br, pos.X, pos.Y, 1, 1) ' create a point 1x1 pixel at point using the fill brush defined above
+
+                ' save back to picturebox to view
+                pBox.Image = bmp
+            Catch ex As Exception
+            End Try
+        End If
+    End Sub
+
+    Private Sub FloodFill(start As Point, ocol As Color)
+        'this doesn't completley work as it wont fill all the edges, but it has the gist of it
+        'i'm guessing this is due to the stack storing multiple of the same points through the itterations.
+        'i tried using recursion but it just resulted in an overflow error every time
+        Dim stk As Stack(Of Point) = New Stack(Of Point)
+        Dim temp As Point
+        Dim count As Integer = 1
+        stk.Push(start)
+        While stk.Count > 0
+            temp = stk.Pop
+            If bmp.GetPixel(temp.X, temp.Y) = ocol Then
+                gr.FillRectangle(br, temp.X, temp.Y, 1, 1)
+
+                stk.Push(New Point(temp.X - 1, temp.Y))
+                stk.Push(New Point(temp.X + 1, temp.Y))
+                stk.Push(New Point(temp.X, temp.Y - 1))
+                stk.Push(New Point(temp.X, temp.Y + 1))
+
+            End If
+
+        End While
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim save As New SaveFileDialog
+        Dim re As Boolean = save.ShowDialog()
+        If re = True Then
+            pBox.Image.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Bmp)
+        End If
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        End
+    End Sub
+End Class
